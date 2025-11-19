76b06bcfe7a692cbd076ae08369f214f47e898bc
James Gibbons <jamesfngibbons@icloud.com>
2025-11-19 12:47:10 -0800

    Updated frontend asset build

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
+
 const Encore = require('@symfony/webpack-encore');
+Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/js/app.js')
-    .addStyleEntry('app', './assets/scss/app.scss')
+    .addStyleEntry('app-style', './assets/less/app.less')
     .enableSingleRuntimeChunk()
-    .cleanOutputPath()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .enableSassLoader()
+    .enableLessLoader()
     .autoProvidejQuery()
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
@@ -19,4 +20,34 @@ Encore
         config.corejs = 3;
     });
 
-module.exports = Encore.getWebpackConfig();
+const config = Encore.getWebpackConfig();
+
+// Configure LESS loader to resolve node_modules
+if (config.module && config.module.rules && Array.isArray(config.module.rules)) {
+    config.module.rules.forEach((rule) => {
+        if (rule && rule.test && rule.test.toString().includes('less')) {
+            if (rule.use && Array.isArray(rule.use)) {
+                rule.use.forEach((use) => {
+                    if (use && use.loader && use.loader.includes('less-loader')) {
+                        use.options = use.options || {};
+                        use.options.lessOptions = use.options.lessOptions || {};
+                        use.options.lessOptions.paths = [
+                            require('path').resolve(__dirname, 'node_modules')
+                        ];
+                    }
+                });
+            } else if (rule.use && typeof rule.use === 'object' && rule.use.loader) {
+                // Handle single use object
+                if (rule.use.loader.includes('less-loader')) {
+                    rule.use.options = rule.use.options || {};
+                    rule.use.options.lessOptions = rule.use.options.lessOptions || {};
+                    rule.use.options.lessOptions.paths = [
+                        require('path').resolve(__dirname, 'node_modules')
+                    ];
+                }
+            }
+        }
+    });
+}
+
+module.exports = config;
